8400500ceeb3554ca5d28bf8a47e5f265f7e9c15
Akmal Bintang <akmalbintang33@gmail.com>
2025-07-04 18:48:46 +0700

    Fetching Data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { Sql } from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
